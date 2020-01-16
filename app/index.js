32565f3f798f5b3a4e6a4ef439eeed5df264e4b9
ferdous--hutum-ubuntu <farhim.ferdous@pridesys.com>
2020-01-16 15:22:41 +0600

    update MESSAGE env value and remove binding to host ports of apps

--- app/index.js
+++ app/index.js
@@ -8,8 +8,8 @@ app.get("/", (req, res) => {
   res.header("Content-Type", "text/html");
   console.log(`Node PID ${process.pid} Handling request of ${req.hostname}...`);
   res.send(`
-    <h1>Node PID: ${process.pid}</h1>
-    Hi ${process.env.MESSAGE}!
+    <h1>Hi ${process.env.MESSAGE}!</h1>
+    Node PID: ${process.pid}
   `);
 });
 
